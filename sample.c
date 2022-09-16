f7045c2d2664a19cf42975bd1a216c7f0f35d873
194g1a0528 <112165444+194g1a0528@users.noreply.github.com>
2022-09-16 16:01:49 +0530

    Divya sample.c

---
+++ sample.c
@@ -0,0 +1,6 @@
+Name Of the Candidate: P.Divya
+   Roll No: 194G1A0528
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 16-09-2022
+   Date of Execution: 16-09-2022
+*/
